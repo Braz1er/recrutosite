37e66f6e36ad7be5f94f7d67b928aa0066702229
brazy <artrem99k5@gmail.com>
2024-02-28 21:48:27 +0300

    ////

--- apppppp.py
+++ apppppp.py
@@ -1,12 +1,11 @@
-from flask import request
-from flask import Flask
+from flask import request, Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_recruto():
     name = request.args.get('name')
     message = request.args.get('message')
-    return f'Hello {name}! {message}!'
+    return render_template('index.html', name=name, message=message)
 
 if __name__ == '__main__':
   app.run()
