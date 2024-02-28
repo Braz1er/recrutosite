1a7394b1d31c386eb4a210e4e39bbc9eaec44e83
brazy <artrem99k5@gmail.com>
2024-02-28 22:26:33 +0300

    ////

--- apppppp.py
+++ apppppp.py
@@ -6,6 +6,3 @@ def hello_recruto():
     name = request.args.get('name')
     message = request.args.get('message')
     return render_template('index.html', name=name, message=message)
-
-if __name__ == '__main__':
-  app.run()
