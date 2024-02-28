3de61f6bf82b84edaa40cc9e140300edbec58504
brazy <artrem99k5@gmail.com>
2024-02-28 23:00:42 +0300

    ////

--- apppppp.py
+++ apppppp.py
@@ -1,12 +1,11 @@
 from flask import request, Flask, render_template
-import jinja2
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     name = request.args.get('name')
     message = request.args.get('message')
-    return render_template('index.html', name=name, message=message)
+    return f'Hello {name}! {message}!'
 
 if __name__ == '__main__':
     app.run()
