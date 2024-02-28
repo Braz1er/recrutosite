f8e97d98ed26e3c6930a95d3c8d4b8b6b5613364
brazy <artrem99k5@gmail.com>
2024-02-28 23:42:47 +0300

    ////

--- apppppp.py
+++ apppppp.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def index():
     name = request.args.get('name')
     message = request.args.get('message')
-    return render_template('index.html', name='арвпрвра', message='fegefgssdf')
+    # name = 'прарпк'
+    # message = 'авпроавоп'
+    return render_template('index.html', name=name, message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
